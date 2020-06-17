9f5e2ed6d04024644070f76104e7314e597d1833
Mohammad Ahmad <mahmad97taha@gmail.com>
2020-06-17 15:41:36 +0800

    some stuff

--- frontend/src/views/events/Create.vue
+++ frontend/src/views/events/Create.vue
@@ -1,41 +1,45 @@
 <template>
   <div>
     <h1>Create Event</h1>
-    <div><b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <!-- <b-form-group
-        id="title-group"
-        label="Event title:"
-        label-for="title"
-        description="What do you want to call your event?"
-        >
-        <b-form-input
-          id="title"
-          v-model="task.title"
-          type="text"
-          required
-          placeholder="Enter title"
-        ></b-form-input></b-form-group> -->
+    <div>
+      <div>
+        <b-form @submit="onSubmit" @reset="onReset">
+          <b-form-group
+          id="title-group"
+          label="Event title:"
+          label-for="title"
+          description="What do you want to call your event?"
+          >
+            <b-form-input
+              id="title"
+              v-model="event.title"
+              type="text"
+              required
+              placeholder="Enter title"
+            ></b-form-input>
+          </b-form-group>
 
-        <!-- <b-form-group
-        id="input-group-2"
-        label="Email address:"
-        label-for="input-2"
-        description="We'll never share your email with anyone else."
-        >
-        </b-form-group>
-     
-        <b-form-group
-        id="input-group-3"
-        label="Email address:"
-        label-for="input-3"
-        description="We'll never share your email with anyone else."
-        >
-        </b-form-group> -->
+          <!-- <b-form-group
+          id="title-group"
+          label="Event title:"
+          label-for="title"
+          description="What do you want to call your event?"
+          >
+            <b-form-input
+              id="title"
+              v-model="event.title"
+              type="text"
+              required
+              placeholder="Enter title"
+            ></b-form-input>
+          </b-form-group> -->
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
 
-    </b-form></div>
+        </b-form>
+      </div>
+    </div>
   </div>
 </template>
 
